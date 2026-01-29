b58ca4dce2b3ae2db93beae6a519041f66e715f7
MAJD <majd9alharbi@gmail.com>
2026-01-29 14:37:20 +0300

    feat: add base api views with ownership enforcement

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def protected_test(request):
     return Response({"message": "JWT authentication works"})
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/protected/', protected_test),
+    path("admin/", admin.site.urls),
+    path("api/token/", TokenObtainPairView.as_view()),
+    path("api/protected/", protected_test),
+    path("api/", include("planner.urls")),
 ]
--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Shift, Commitment, RecoveryRule
+from .serializers import (
+    ShiftSerializer,
+    CommitmentSerializer,
+    RecoveryRuleSerializer,
+)
 
-# Create your views here.
+
+class BaseOwnedModelViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ShiftViewSet(BaseOwnedModelViewSet):
+    model = Shift
+    serializer_class = ShiftSerializer
+
+
+class CommitmentViewSet(BaseOwnedModelViewSet):
+    model = Commitment
+    serializer_class = CommitmentSerializer
+
+
+class RecoveryRuleViewSet(BaseOwnedModelViewSet):
+    model = RecoveryRule
+    serializer_class = RecoveryRuleSerializer
